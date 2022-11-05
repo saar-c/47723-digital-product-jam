f1d049d55eb313b63362d68829af276173bf8a5d
unknown <saco@tutanota.com>
2022-11-05 23:02:04 +0200

    finished

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-html_tag_list = document.getElementById("html_tag_list")
-hide_html_tag_list_button = document.getElementById("hide_html_tag_list_button");
-
-function hide_element(id){
-  document.getElementById(id).style.visibility = "hidden";
+function hide_show_element(id) {
+  element = document.getElementById(id);
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  }
+  else {
+    element.style.display = "none";
+  }
 }
 
-hide_html_tag_list_button.addEventListener("click", hide_element(html_tag_list));
+function change_background_color(color) {
+  document.body.style.background = color;
+}
+
+function change_text_color(color) {
+  document.body.style.color = color;
+}
+
+document.getElementById("hide_html_tag_list_button").addEventListener(
+  "click", function() { hide_show_element("html_tag_list") }
+);
+
+document.getElementById("bg_color_change_button").addEventListener(
+  "click", function() { change_background_color("white") }
+);
+
+document.getElementById("text_color_change_button").addEventListener(
+  "click", function() { change_text_color("black") }
+);
