810387093ecfa47d691fe6d252a0120e63f86d27
unknown <saco@tutanota.com>
2022-11-05 22:00:59 +0200

    hide html tag list

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-contactButton = document.querySelector("#contact button");
+html_tag_list = document.getElementById("html_tag_list")
+hide_html_tag_list_button = document.getElementById("hide_html_tag_list_button");
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
-});
+function hide_element(id){
+  document.getElementById(id).style.visibility = "hidden";
+}
+
+hide_html_tag_list_button.addEventListener("click", hide_element(html_tag_list));
